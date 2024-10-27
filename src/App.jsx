94509df458c3cd28cd83c783e76517a99fe40908
Duy Tran <duytran2310@gmail.com>
2024-10-26 20:57:27 -0500

    comment out example code

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,18 @@ function getRandInt(max) {
   return Math.floor(Math.random() * (max+1));
 }
 
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 
-function Counter() {
-  const [count, setCount] = useState(0);
+// function Counter() {
+//   const [count, setCount] = useState(0);
 
-  return (
-    <div>
-      <p>Count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <p>Count: {count}</p>
+//       <button onClick={() => setCount(count + 1)}>Increment</button>
+//     </div>
+//   );
+// }
 
 
 function Header() {
